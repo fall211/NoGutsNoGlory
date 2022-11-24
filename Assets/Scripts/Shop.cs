35589b964442f79e004a0b22be7df03266a5c49d
fall211 <94519579+fall211@users.noreply.github.com>
2022-11-24 10:53:55 -0800

    selling shit is now possible

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -28,6 +28,33 @@ public class Shop : MonoBehaviour
 
     }
 
+public void sell_heart() {
+    if (inventory_script.fish_inventory[0] > 0) {
+        inventory_script.fish_inventory[0] -= 1;
+        inventory_script.net_worth += 1;
+    } else {
+        Debug.Log("You don't have any hearts to sell!");
+    }
+}
+
+public void sell_guts() {
+    if (inventory_script.fish_inventory[1] > 0) {
+        inventory_script.fish_inventory[1] -= 1;
+        inventory_script.net_worth += 1;
+    } else {
+        Debug.Log("You don't have any guts to sell!");
+    }
+}
+
+public void sell_lung() {
+    if (inventory_script.fish_inventory[2] > 0) {
+        inventory_script.fish_inventory[2] -= 1;
+        inventory_script.net_worth += 1;
+    } else {
+        Debug.Log("You don't have any lungs to sell!");
+    }
+}
+
     private bool check_player_proximity() {
         return Vector2.Distance(player_obj.transform.position, transform.position) <= 4f;
     }
